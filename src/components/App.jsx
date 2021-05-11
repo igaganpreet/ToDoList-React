9427c4ab4808972506cf9b829c3f9e2faf0aa98b
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-12 00:58:37 +0530

    add item to list on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setItem] = useState("");
+  const [itemText, setItemText] = useState([]);
+
+  function handleChange(event) {
+    let newValue = event.target.value;
+    setItem(newValue);
+  }
+  function handleClick(event) {
+    console.log("clicked");
+    setItemText((prevValue) => {
+      return [...prevValue, inputText];
+    });
+    // console.log(itemText);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="inputName"
+          value={inputText}
+        />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {itemText.map((item) => {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
