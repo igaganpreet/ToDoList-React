6d12541baabb3215fc2ae2d5b1f507d74c0cbbbc
Gagan Preet <gaganpreet.dev@gmail.com>
2021-05-12 22:09:40 +0530

    strike through and check and uncheck item

--- src/components/ToDoItems.jsx
+++ src/components/ToDoItems.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoItems(props) {
-  return <li>{props.value}</li>;
+  const [styleDescription, updateStyle] = useState({ textDecoration: "none" });
+  const [decorated, updateState] = useState(false);
+  function handleClick(event) {
+    console.log("clicked");
+    if (decorated === false) {
+      updateStyle({ textDecoration: "line-through" });
+      updateState(true);
+    } else if (decorated === true) {
+      updateStyle({ textDecoration: "none" });
+      updateState(false);
+    }
+  }
+
+  return (
+    <div>
+      <li style={styleDescription} onClick={handleClick}>
+        {props.value}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItems;
